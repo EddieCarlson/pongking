755b00bea6e16c1f8119f0eb01fb3fdd3ae9bc6f
Riley Strong <strongriley@gmail.com>
2011-03-27 23:15:39 -0500

    django evolution in place

--- pongking/settings.py
+++ pongking/settings.py
@@ -95,6 +95,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django_evolution',
 	'pong',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
